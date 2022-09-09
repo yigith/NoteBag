1c2826e835dd8eeb14b72631919726b419f94a39
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-09-09 13:02:10 +0300

    react ui

--- note-bag/src/App.js
+++ note-bag/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import NbNavbar from './components/NbNavbar';
+import { Form, Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  // App bileşeni ilk render olduğunda (mount)
+  useEffect(function() {
+    axios.get("https://localhost:7292/api/Notes")
+      .then(function (response) {
+        setNotes(response.data);
+      });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NbNavbar />
+      <Container fluid className="mt-4">
+        <Row>
+          <Col sm={4} md={3}>
+            <Button>Yeni</Button>
+            <ListGroup className="mt-3 mb-3">
+              { notes.map((note) => 
+                <ListGroup.Item action key={note.id}>
+                  {note.title}
+                </ListGroup.Item>
+              )}
+            </ListGroup>
+          </Col>
+          <Col sm={8} md={9}>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Title" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control as="textarea" placeholder="Content" rows={10} />
+              </Form.Group>
+            </Form>
+            <div>
+              <Button variant="primary" className="me-2">Kaydet</Button>
+              <Button variant="danger">Sil</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ note-bag/src/components/NbNavbar.js
@@ -0,0 +1,20 @@
+import {  Container, Nav, Navbar } from 'react-bootstrap';
+
+function NbNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NbNavbar;
